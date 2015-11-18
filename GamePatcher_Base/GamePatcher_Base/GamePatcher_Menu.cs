d8c3813ab6fcc63a00ad8713c62c6b7f663956a4
Marcin <f1time@o2.pl>
2015-11-18 19:13:49 +0100

    GamePatcher_Base Project created

---
+++ GamePatcher_Base/GamePatcher_Base/GamePatcher_Menu.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GamePatcher_Base
+{
+    public partial class GamePatcher_Menu : Form
+    {
+        public GamePatcher_Menu()
+        {
+            InitializeComponent();
+        }
+    }
+}
