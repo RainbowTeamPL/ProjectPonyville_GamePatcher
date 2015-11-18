71f8dbb11b3e2927940748de272665269b979147
Marcin <f1time@o2.pl>
2015-11-18 19:24:07 +0100

    Summary N/A

--- GamePatcher_Base/GamePatcher_Base/Program.cs
+++ GamePatcher_Base/GamePatcher_Base/Program.cs
@@ -16,7 +16,7 @@ namespace GamePatcher_Base
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new GamePatcher_Menu());
         }
     }
 }
